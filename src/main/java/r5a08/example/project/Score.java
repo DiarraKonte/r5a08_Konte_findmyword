ec8d450a8c23a28fb36ba3e786564acda22b457f
DiarraKonte <diarrakontepro@gmail.com>
2025-10-01 11:57:20 +0200

    mise a jour

---
+++ src/main/java/r5a08/example/project/Score.java
@@ -0,0 +1,12 @@
+package r5a08_findmyword;
+
+public class Score {
+    private Letter[] letters;
+
+    public Score(){
+    }
+
+    public Letter letter(int i) {
+        return null;
+    }
+}
---
+++ src/test/java/r5a08/example/project/WordTest.java
@@ -0,0 +1,21 @@
+package r5a08_findmyword;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WordTest {
+
+    @Test
+    void should_check_one_incorrect_letter(){
+        // Arrange
+        Word word = new Word("E");
+
+        // Act
+        Score score = word.guess("C"); // tentative incorrecte
+
+        // Assert
+        assertEquals("E", score.letter(0));
+    }
+
+}
